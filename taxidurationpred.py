0d16abdde8a27588e3051a618a2cf7de234885a4
Shruthi Ramakrishnan <shruthi2811@gmail.com>
2017-09-15 10:24:34 -0400

    Update taxidurationpred.py

--- taxidurationpred.py
+++ taxidurationpred.py
@@ -2,11 +2,19 @@
 """
 Created on Thu Sep 14 22:08:24 2017
 
-@author: Administrator
+@author: Shruthi Ramakrishnan
 """
-
+#Reading train and test dataset
 import pandas as pd
-train = pd.read_csv('S:\Kaggle\Taxi Duration\train\train.csv')
-#test = pd.read_csv('Data/test.zip', compression='zip',parse_dates=['pickup_datetime'])
-print(train.head())
-print(train.info())
+train = pd.read_csv('S:\\Kaggle\\Taxi\\train\\train.csv')
+test = pd.read_csv('S:\\Kaggle\\Taxi\\test\\test.csv')
+
+#removind the dropoff time and trip duration from the dataset
+train_new = train.drop('trip_duration',1)
+train_new = train_new.drop('dropoff_datetime',1)
+predictedvalue = train['trip_duration']
+dropof_training = train['dropoff_datetime']
+
+#merge train and test dataset for data cleaning
+merged_data = [train_new , test] 
+finaldata = pd.concat(merged_data)
